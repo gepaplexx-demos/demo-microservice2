0e10b5ccf8100e8a81bb5866635ac2d8087d483b
Alexander Schober <alex.schober1@gmx.at>
2022-06-08 08:53:19 +0200

    GPX-262 überflüssiger Code entfernt

--- src/main/java/at/gepardec/rest/EntrypointResource.java
+++ src/main/java/at/gepardec/rest/EntrypointResource.java
@@ -47,9 +47,7 @@ public class EntrypointResource {
                 Log.info("[" + transactionID.toString() + "]" + " Stopping OrderedCallService...\n\n");
                 return;
         }
-
         Log.info("[" + transactionID.toString() + "]" + " - Calling next Service");
-
         orderedCallService.callServiceBySequence(orderSequence, transactionID);
     }
 }
