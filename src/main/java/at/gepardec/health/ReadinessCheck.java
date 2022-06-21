790170729983ea9203c4394958df8a9e87703518
Alexander Schober <alex.schober1@gmx.at>
2022-06-21 14:48:15 +0200

    testing cicd-pipeline => getting cpu usage

--- src/main/java/at/gepardec/health/ReadinessCheck.java
+++ src/main/java/at/gepardec/health/ReadinessCheck.java
@@ -3,10 +3,13 @@ package at.gepardec.health;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 
+import com.sun.management.OperatingSystemMXBean;
 import org.eclipse.microprofile.health.HealthCheck;
 import org.eclipse.microprofile.health.HealthCheckResponse;
 import org.eclipse.microprofile.health.Readiness;
 import org.jboss.logging.Logger;
+
+import java.lang.management.ManagementFactory;
 
 
 @Readiness
@@ -16,10 +19,14 @@ public class ReadinessCheck implements HealthCheck {
     @Inject
     Logger Log;
 
+    OperatingSystemMXBean osBean = ManagementFactory.getPlatformMXBean(OperatingSystemMXBean.class);
+
     @Override
     public HealthCheckResponse call() {
         Log.info("Calling Readiness-Check: ");
         Log.info("Test");
+        Log.info("CPu-Usage: " + osBean.getProcessCpuLoad());
+        Log.info("CPu-Usage_System: " + osBean.getSystemLoadAverage());
         return HealthCheckResponse.up("Service2");
     }
 }
